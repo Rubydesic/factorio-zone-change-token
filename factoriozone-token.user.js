be7a14ac02801776f5cfdd715efe0c35106e9897
Rubydesic <rubydesic@gmail.com>
2021-10-26 09:44:12 -0400

    Add a collapse button, work on valheim

--- factoriozone-token.user.js
+++ factoriozone-token.user.js
@@ -1,17 +1,48 @@
 // ==UserScript==
 // @name         Factorio Zone Token
-// @version      0.3
+// @version      0.4
 // @description  Set user token on factorio.zone
 // @author       Rubydesic
 // @match        https://factorio.zone/
+// @match        https://valheim.zone/
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2@10
 // @downloadURL  https://raw.githubusercontent.com/Rubydesic/factorio-zone-change-token/master/factoriozone-token.user.js
 // ==/UserScript==
 
 (function () {
-    const node = document.createElement("style")
-    node.innerText = `
+    const USER_TOKEN_KEY = 'userToken'
+    const TOKEN_HISTORY_KEY = '_rubydesicTokenHistory'
+    const COLLAPSE_HISTORY_KEY = '_rubydesicCollapseHistory-'
+
+    function makeCollapseButton(el, desc, id) {
+        const button = document.createElement('button')
+        button.classList.add('collapse-control', 'pure-button')
+        button.innerText = 'Hide' + desc
+        if (localStorage.getItem(COLLAPSE_HISTORY_KEY + id) === 'hidden') {
+            el.classList.add('hidden')
+            el.classList.remove('active')
+        }
+        button.addEventListener('click', () => {
+            const cl = el.classList
+            if (cl.contains('hidden')) {
+                cl.add('active')
+                cl.remove('hidden')
+                button.innerText = 'Hide' + desc
+                localStorage.setItem(COLLAPSE_HISTORY_KEY + id, 'visible')
+            } else {
+                cl.add('hidden')
+                cl.remove('active')
+                button.innerText = 'Show' + desc
+                localStorage.setItem(COLLAPSE_HISTORY_KEY + id, 'hidden')
+            }
+        })
+        el.classList.add('collapse')
+        return button
+    }
+
+    const node = document.createElement('style')
+    node.innerHTML = `
         #tokenHistoryTable {
             width: 100%;
         }
@@ -28,28 +59,41 @@
         .token-control > * {
             margin: 6px;
         }
-    `;
+        section.output-container {
+            min-height: 20em;
+        }
+        .collapse-control {
+            margin: 6px;
+        }
+        .collapse.hidden {
+            display: none;
+        }
+        .collapse.visible {
+            display: block;
+        }
+    `
     document.head.appendChild(node)
 
-    const USER_TOKEN_KEY = "userToken"
-    const TOKEN_HISTORY_KEY = "_rubydesicTokenHistory"
-
-    const header = document.querySelector("section.info")
     const currentToken = localStorage.getItem(USER_TOKEN_KEY)
-    header.innerHTML += `
+    const controlContainer = document.querySelector('section.control-container')
+    controlContainer.insertAdjacentHTML('beforebegin', `
         <div class="token-control flex-row">
             <button class="pure-button" id="changeToken">Change Token</button>
             <button class="pure-button" id="tokenHistory">Token History</button>
-            ${currentToken ? `<p>Your token is <strong id="currentToken">${currentToken}</strong></p>` : ""}
+            ${currentToken ? `<p>Your token is <strong id="currentToken">${currentToken}</strong></p>` : ''}
         </div>
-    `;
+    `)
 
+    const infoCollapse = makeCollapseButton(document.querySelector('section.info'), ' Info', 'info')
+    document.querySelector('.token-control').insertAdjacentElement('afterbegin', infoCollapse)
+    const tokenCollapse = makeCollapseButton(document.querySelector('.token-control > p'), '', 'token')
+    document.querySelector('.token-control').insertAdjacentElement('beforeend', tokenCollapse)
 
-    document.getElementById("tokenHistory").onclick = async () => {
-        const history = JSON.parse(localStorage.getItem(TOKEN_HISTORY_KEY));
+    document.getElementById('tokenHistory').onclick = async () => {
+        const history = JSON.parse(localStorage.getItem(TOKEN_HISTORY_KEY))
 
         if (history === null) {
-            await Swal.fire({ title: "No history!" })
+            await Swal.fire({ title: 'No history!' })
         } else {
             const tableRows = history
                 .map(e => `
@@ -58,11 +102,11 @@
                         <td>${e.token}</td>
                     </tr>
                 `)
-                .join("");
+                .join('')
 
             await Swal.fire({
-                titleText: "Token History",
-                width: "38rem",
+                titleText: 'Token History',
+                width: '38rem',
                 html: `
                     <table id="tokenHistoryTable">
                         <tr>
@@ -76,18 +120,18 @@
         }
     }
 
-    document.getElementById("changeToken").onclick = async () => {
+    document.getElementById('changeToken').onclick = async () => {
         const originalToken = localStorage.getItem(USER_TOKEN_KEY)
         const newToken = (await Swal.fire({
-            input: "text",
+            input: 'text',
             inputValue: originalToken,
-            title: "Set Token",
-            text: "Type reset for a new token"
+            title: 'Set Token',
+            text: 'Type reset for a new token'
         })).value
 
         if (newToken === null) return
 
-        if (newToken.toLowerCase() === "reset") {
+        if (newToken.toLowerCase() === 'reset') {
             localStorage.removeItem(USER_TOKEN_KEY)
             location.reload()
         } else if (newToken !== originalToken || originalToken === null) {
